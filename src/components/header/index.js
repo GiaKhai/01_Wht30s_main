13726eca4da04e311cf2f5e2947ad3136e9cc59b
giakhai <khaigia11111@gmail.com>
2021-11-09 23:45:13 +0700

    done test

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../image/logo.png";
 import "./header.css";
 
 const Header = () => {
+  const [state, setState] = useState({
+    menu: false,
+    isOpen: false,
+    homeLinkClass: "nav-item nav-link",
+    aboutLinkClass: "nav-item nav-link",
+    menuClass: "",
+  });
+
+  const toggleMenu = () => {
+    setState({
+      ...state,
+      menu: !state.menu,
+    });
+  };
+
+  const show = state.menu ? "show" : "";
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark header">
       <div className="container">
         <a className="navbar-brand" href="/">
           <img className="logo" src={logo} alt="" />
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="/navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+          <span className="navbar-toggler-icon" />
         </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className={"collapse navbar-collapse " + show}>
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <a className="nav-link" href="/">
