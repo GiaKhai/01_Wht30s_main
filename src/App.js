b68f9da13c80081e999f104daf36fc9008fc2b10
giakhai <khaigia11111@gmail.com>
2021-11-09 11:13:11 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Banner from "./components/banner";
 import BestProduct from "./components/best-product";
-// import Header from "./components/header";
+import Header from "./components/header";
 import Introduction from "./components/intro";
 import Product from "./components/product";
 import BrandStory from "./components/stoty";
@@ -9,7 +9,7 @@ import BrandStory from "./components/stoty";
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Banner />
       <Introduction />
       <BestProduct />
