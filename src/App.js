57d333db9a4d6480069a9061c5ebe8ca62137efd
giakhai <khaigia11111@gmail.com>
2021-11-09 08:27:57 +0700

    fix deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Banner from "./components/banner";
 // import BestProduct from "./components/best-product";
-import Header from "./components/header";
+// import Header from "./components/header";
 import Introduction from "./components/intro";
 import Product from "./components/product";
 import BrandStory from "./components/stoty";
